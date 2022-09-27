1096a7a8703e232fd87f703604d9b108dcdb3f9c
Miguel Robles <miguel.robles@atmosfera.unam.mx>
2022-09-26 23:01:37 -0500

    se incorporó prep_wrf en download.py

--- download.py
+++ download.py
@@ -4,6 +4,8 @@ import datetime as dt
 from os.path import exists
 from os.path import join
 from os import scandir
+from netCDF4 import Dataset
+import numpy as np
 
 #configura parser para filtrar los enlaces a los datos
 # en un catalogo THREDDS
@@ -86,4 +88,52 @@ def get_req(remoto):
     with open(filename,'wb') as fp:
         fp.write(page.content)
     print(filename, 'guardado')
+
+def prep_wrf(filename):
+    print('procesando:', filename)
+    with  Dataset(filename, 'r', format='NETCDF4_CLASSIC') as ds:
+        data_u = ds['U10'][:]
+        data_v = ds['V10'][:]
+        data_lat = ds['XLAT'][0,:,0]
+        data_lon = ds['XLONG'][0,0,:]
+        secs = ds['Time'][:]
+
+    time = []
+    for s in secs:
+        time.append(dt.datetime.fromtimestamp(s, dt.timezone.utc))
+    t_units = time[0].strftime("hours since %Y-%m-%d %H:%M:%S UTC")
+    for t in time:
+        hours.append((t-time[0]).total_seconds()/3600)
+    hours = np.array(hours)
+    fillValue = 1.267651e+30
+    with Dataset(filename, 'w', format='NETCDF3_CLASSIC') as dataset:
+        dataset.createDimension('time', hours.shape[0])
+        dataset.createDimension('lat', data_lat.shape[0])
+        dataset.createDimension('lon', data_lon.shape[0])
+        time = dataset.createVariable('time', np.float64, ('time', ))
+        lon = dataset.createVariable('lon', np.float32, ('lon', ))
+        lat = dataset.createVariable('lat', np.float32, ('lat', ))
+        u = dataset.createVariable('u', (np.float32), ('time', 'lat', 'lon'), fill_value=fillValue)
+        v = dataset.createVariable('v', (np.float32), ('time', 'lat', 'lon'), fill_value=fillValue)
+        dataset.grid_type = 'REGULAR'
+        lat.long_name = 'Latitude'
+        lat.units = 'degrees_north'
+        lat.standard_name = 'latitude'
+        lon.long_name = 'Longitude'
+        lon.units = 'degrees_east'
+        lon.standard_name = 'longitude'
+        time.long_name = 'Time'
+        time.units = t_units
+        time.standard_name = 'time'
+        u.long_name = 'Eastward Air Velocity'
+        u.standard_name = 'eastward_wind'
+        u.units = 'm/s'
+        v.long_name = 'Northward Air Velocity'
+        v.standard_name = 'northward_wind'
+        v.units = 'm/s'
+        lat[:] = data_lat
+        lon[:] = data_lon
+        time[:] = hours
+        u[:] = data_u
+        v[:] = data_v
 
